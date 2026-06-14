da60a443a821f1c56dc47c02629fcb99e80f1b25
Bryan <bryanfinesw@gmail.com>
2026-06-14 07:01:35 -0400

    Feature: Added pasport serialization

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const express = require("express");
 const path = require("node:path");
+const pool = require("./db/pool.js");
 
 // Authentication
 const session = require("express-session");
@@ -31,6 +32,41 @@ app.use(session({ secret: process.env.SECRET, resave: false, saveUninitialized: 
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(assetsPath));
+app.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+            const user = rows[0];
+
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+
+            if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+        const user = rows[0];
+
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
 
 // Routes
 app.use("/", indexRouter);
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,6 +2,8 @@ const db = require("../db/queries");
 const links = [
     { href: "/", text: "Home" },
     { href: "/messages", text: "Messages" },
+    { href: "/sign-up", text: "Sign up" },
+    { href: "/log-in", text: "Log in" },
 ]
 
 async function getIndex(req, res) {
